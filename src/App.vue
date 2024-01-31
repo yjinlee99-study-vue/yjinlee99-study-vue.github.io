06984a8090d4f63321ffb621d813f1f8d20a2bb5
yjinnote <yjinlee99@gmail.com>
2024-01-31 11:39:48 +0900

    hello count + rev name

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
-import { ref } from 'vue'
-const count = ref(0)
+import { ref, onMounted } from 'vue'
+
+const count = ref({
+	cnt: 0,
+	name: "HELLO"
+})
+
+function increment() {
+	count.value.cnt = count.value.cnt + 1
+	count.value.name = count.value.name.split('').reverse().join('')
+}
+
+onMounted(()=> {
+	console.log(`initial count is ${count.value}.`)
+})
 </script>
 
 <template>
-	<button @click="count++">Count is: {{ count }}</button>
+	<button @click="increment">Count is: {{ count.cnt }} {{ count.name }}</button>
 </template>
-
-<style scoped>
-button {
-	font-weight: bold;
-}
-</style>
