a68ab0166b44b1be695e6c741dfa256e99ca83b1
yjinnote <yjinlee99@gmail.com>
2024-01-31 18:11:59 +0900

    주문력이 5 보다 큰 챔피언 목록 출력

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,8 @@ export default {
 
 <template>
   <div>
-  	<ul>
-      <li v-for="c in champions.data">
+  	<ul v-for="c in champions.data">
+      <li v-if="c.info.magic > 5" >
 	<p>{{ c.name }}</p>
 	<p>{{c.title}} <br> {{c.blurb}}</p>
 	<span>공격력: {{ c.info.attack }}   방어력: {{ c.info.defense }}   주문력: {{ c.info.magic }}   포지션: {{ c.tags }} </span><br><br>
