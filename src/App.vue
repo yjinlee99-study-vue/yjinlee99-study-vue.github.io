7b398eea53721b3b5d8ae7fb0f31b3975988e06d
yjinnote <yjinlee99@gmail.com>
2024-01-31 17:57:38 +0900

    모든 목록 출력 수정

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,9 @@ export default {
   <div>
   	<ul>
       <li v-for="c in champions.data">
-	<strong>{{ c.name }}</strong><br>
+	<p>{{ c.name }}</p>
 	<p>{{c.title}} <br> {{c.blurb}}</p>
-	<span>공격력 {{ c.info.attack }} 방어력 {{ c.info.defense }} 주문력 {{ c.info.magic }} 포지션 {{ c.tags }} </span><br><br>
+	<span>공격력: {{ c.info.attack }}   방어력: {{ c.info.defense }}   주문력: {{ c.info.magic }}   포지션: {{ c.tags }} </span><br><br>
       </li>
     </ul>
   </div>
