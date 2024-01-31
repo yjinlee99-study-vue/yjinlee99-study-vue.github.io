4bb9b284eb506ccde75a4b9d76c3063b6bdb1272
yjinnote <yjinlee99@gmail.com>
2024-01-31 18:32:47 +0900

    리가 들어가는 챔치언 목록 출력

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
 <template>
   <div>
   	<ul v-for="c in champions.data">
-      <li v-if="c.info.magic > 5" >
+      <li v-if="c.name.includes('리')" >
 	<p>{{ c.name }}</p>
 	<p>{{c.title}} <br> {{c.blurb}}</p>
 	<span>공격력: {{ c.info.attack }}   방어력: {{ c.info.defense }}   주문력: {{ c.info.magic }}   포지션: {{ c.tags }} </span><br><br>
